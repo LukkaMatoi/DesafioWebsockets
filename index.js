bf45299db7d5d9c45f4adb9bc32f592cb89e9a14
Lukka <lucaraime@gmail.com>
2022-02-16 23:02:16 -0300

    entregaProyecto

--- index.js
+++ index.js
@@ -50,6 +50,7 @@ let productos =
 /*-----------------------------------------------------------------*/
 
 
+/*-------------------------WEBSOCKET-------------------------------*/
 const mensajes = [];
 
 io.on('connection', socket => {
@@ -67,10 +68,60 @@ io.on('connection', socket => {
 });
 
 
+/*-----------------------------------------------------------------*/
+
+
+
+/*----------------------------PAGINAS------------------------------*/
 
 app.get('/', (req, res)=>{
     res.render('inicio', {productos: productos});
 });
+
+
+
+/*---------------------------------*/
+
+
+
+router.get("/productos",(req,res)=>{
+
+
+  
+  
+  res.send(productos);
+});
+
+
+
+/*---------------------------------*/
+
+
+
+router.get("/productos/:id",(req,res)=>{
+
+  function getById(id){
+
+    const filtro = productos.find((filtro) => filtro.id == id);
+   
+          if(filtro !== undefined){
+              console.log(filtro)
+              console.log("-----------------")
+              res.send(filtro)
+              
+          }else{
+              console.log(`no se encontro un producto para el id ${id}`)
+              return null
+          }}
+    
+
+    getById(req.params.id)
+
+})
+
+
+
+/*---------------------------------*/
 
 
 
@@ -99,13 +150,113 @@ app.post('/save', (req,res) => {
 
 
 
-;
+/*---------------------------------*/
+
+
+
+router.delete("/productos/:id",(req,res)=>{
+  function deleteById(id){
+ 
+    let valor = id
+    
+    productos = productos.filter(item => item.id != valor)
+    res.send(productos)
+}
+deleteById(req.params.id)
+
+});
+
+
+
+/*---------------------------------*/
+
+
+
+router.put("/productos/:id",(req,res)=>{
+
+
+  function editById(title,price,id){
+
+    let filtro = productos.find((filtro) => filtro.id == id);
+  
+    filtro = {
+      title: title,
+      price: price,
+      id: id
+    }
+  res.send(filtro)
+  }
+  
+ editById("nombre Editado","numero editado",req.params.id)
+
+});
+
+
+
+/*---------------------------------*/
+
+let inCart = []
+
+router.get("/carrito",(req,res)=>{
+
+
+
+res.render("carro",{productos:productos})
+
+});
+
+
+
+
+
+
+
+
+
+app.post('/incart', (req,res) => {
+  let nuevoItem = {
+    
+    title: req.body.title,
+    price: req.body.price,
+
+  }
+
+  inCart.push(nuevoItem)
+
+  let productosString = JSON.stringify(inCart)
+
+  res.send(`<body>
+    <h1>Tu Carrito</h1>
+    <p>${productosString}</p>
+    <a href="/api/carrito">agregar otro producto</a>
+  </body>`)
+  
+
+  console.log("funciona")
+})
+
+
+
+
+
+
+
 const server = httpServer.listen(PORT, ()=>{
     console.log(`Servidor escuchando en puerto ${PORT}`);
 })
 
 
+
+/*---------------------------------*/
+
+
+
 server.on('error', error=>{
     console.error(`Error en el servidor ${error}`);
 });
+
+
+
+/*---------------------------------*/
+
 
